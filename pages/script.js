dcee92f71850c4f9c98f047a19abd417bf330581
Vinicius Torres <129467911+viniciuselt@users.noreply.github.com>
2023-04-23 05:56:20 -0300

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -5,35 +5,37 @@ const correctAnswersDisplay = document.getElementById('correct-answers');
 const correctAnswers = ['a', 'e', 'c', 'b', 'b', 'c'];
 
 form.addEventListener('submit', event => {
-event.preventDefault();
-let score = 0;
-const userAnswers = [
-	form.q1.value,
-	form.q2.value,
-    form.q3.value,
-    form.q4.value,
-    form.q5.value,
-    form.q6.value
-];
+	event.preventDefault();
 
-userAnswers.forEach((answer, index) => {
-	if (answer === correctAnswers[index]) {
-		score += 1;
+	let score = 0;
+	const userAnswers = [
+        form.q1.value,
+        form.q2.value,
+        form.q3.value,
+        form.q4.value,
+        form.q5.value,
+        form.q6.value
+	];
+
+	const incorrectAnswers = [];
+
+	userAnswers.forEach((answer, index) => {
+		if (answer === correctAnswers[index]) {
+			score += 1;
+		} else {
+			incorrectAnswers.push(index + 1);
+		}
+	});
+
+	scoreDisplay.innerText = `Você acertou ${score} de ${correctAnswers.length} perguntas.`;
+
+	if (incorrectAnswers.length === 0) {
+		correctAnswersDisplay.innerText = 'Parabéns! Você acertou todas as perguntas.';
+	} else {
+		const incorrectAnswersText = incorrectAnswers.map(num => `Pergunta ${num}`).join(', ');
+		correctAnswersDisplay.innerText = `As perguntas que você errou foram: ${incorrectAnswersText}.`;
 	}
-});
 
-scoreDisplay.innerText = `Você acertou ${score} de ${correctAnswers.length} perguntas.`;
-
-const incorrectAnswers = userAnswers.filter((answer, index) => {
-	return answer !== correctAnswers[index];
-});
-
-if (incorrectAnswers.length === 0) {
-	correctAnswersDisplay.innerText = 'Parabéns! Você acertou todas as perguntas.';
-} else {
-	correctAnswersDisplay.innerText = `As perguntas que você errou foram: ${incorrectAnswers.join(', ')}.`;
-}
-
-form.style.display = 'none';
-document.getElementById('results').style.display = 'block';
+	form.style.display = 'none';
+	document.getElementById('results').style.display = 'block';
 });
