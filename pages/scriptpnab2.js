ffab0625ebfdab7105c0037a0108e6a5c45d3e03
Vinicius Torres <viniciuseduardolimatorres@gmail.com>
2023-07-14 23:49:21 -0300

    update pnab

---
+++ pages/scriptpnab2.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('quiz-form');
+const scoreDisplay = document.getElementById('score');
+const correctAnswersDisplay = document.getElementById('correct-answers');
+
+const correctAnswers = ['a', 'c', 'b', 'd', 'b', 'e', 'b', 'e', 'c'];
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+
+	let score = 0;
+	const userAnswers = [
+        form.q1.value,
+        form.q2.value,
+        form.q3.value,
+        form.q4.value,
+        form.q5.value,
+        form.q6.value,
+        form.q7.value,
+        form.q8.value,
+        form.q9.value
+	];
+
+	const incorrectAnswers = [];
+
+	userAnswers.forEach((answer, index) => {
+		if (answer === correctAnswers[index]) {
+			score += 1;
+		} else {
+			incorrectAnswers.push(index + 1);
+		}
+	});
+
+	scoreDisplay.innerText = `Você acertou ${score} de ${correctAnswers.length} perguntas.`;
+
+	if (incorrectAnswers.length === 0) {
+		correctAnswersDisplay.innerText = 'Parabéns! Você acertou todas as perguntas.';
+	} else {
+		const incorrectAnswersText = incorrectAnswers.map(num => `Pergunta ${num}`).join(', ');
+		correctAnswersDisplay.innerText = `As perguntas que você errou foram: ${incorrectAnswersText}.`;
+	}
+
+	form.style.display = 'none';
+	document.getElementById('results').style.display = 'block';
+});
